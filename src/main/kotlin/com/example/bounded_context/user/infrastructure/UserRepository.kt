3470dcc4c2fbe4f6a60377951a4a5ba84851e9d5
Hiroaki Date <mmst3l4@gmail.com>
2023-03-30 20:32:49 +0900

    Userを作成するAPIを実装

--- src/main/kotlin/com/example/bounded_context/user/infrastructure/UserRepository.kt
+++ src/main/kotlin/com/example/bounded_context/user/infrastructure/UserRepository.kt
@@ -6,11 +6,25 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 
 interface UserRepository {
+    fun create(name: String, email: String): Unit
     fun findAll(): List<User>
 }
 
 @Repository
 class UserRepositoryImpl(private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate): UserRepository {
+
+    override fun create(name: String, email: String): Unit {
+        val sql = """
+            INSERT INTO
+                users (name, email)
+            VALUES (:name, :email);
+        """.trimIndent()
+        val sqlParams = MapSqlParameterSource()
+            .addValue("name", name)
+            .addValue("email", email)
+        namedParameterJdbcTemplate.update(sql, sqlParams)
+        return
+    }
 
     override fun findAll(): List<User> {
         val sql = "select * from users"
@@ -20,7 +34,7 @@ class UserRepositoryImpl(private val namedParameterJdbcTemplate: NamedParameterJ
             User(
                 it["id"].toString().toInt().toLong(),
                 it["name"].toString(),
-                it["address"].toString(),
+                it["email"].toString(),
             )
         }
     }
--- src/main/kotlin/com/example/bounded_context/user/presentation/UsersController.kt
+++ src/main/kotlin/com/example/bounded_context/user/presentation/UsersController.kt
@@ -3,6 +3,8 @@ package com.example.bounded_context.user.presentation
 import com.example.bounded_context.user.domain.User
 import com.example.bounded_context.user.infrastructure.UserRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,4 +14,15 @@ class UsersController(val userRepository: UserRepository) {
     fun index(): List<User> {
         return userRepository.findAll()
     }
+
+    @PostMapping("/users")
+    fun create(@RequestBody request: CreateUserRequest) {
+        userRepository.create(request.name, request.email)
+        return
+    }
 }
+
+data class CreateUserRequest(
+    val name: String,
+    val email: String
+)
