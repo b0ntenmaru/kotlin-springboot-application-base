1a9965482c5114ad7ba012cc6aa718fcb4bf9fef
Hiroaki Date <mmst3l4@gmail.com>
2023-04-02 01:19:00 +0900

    デバッグ用エンドポイントを実装

--- src/main/kotlin/com/example/bounded_context/authentication/presentational/UserAuthController.kt
+++ src/main/kotlin/com/example/bounded_context/authentication/presentational/UserAuthController.kt
@@ -4,6 +4,7 @@ import com.example.bounded_context.authentication.infrastructure.UserAuth
 import com.example.bounded_context.authentication.infrastructure.UserAuthRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +14,14 @@ class UserAuthController(
     val userAuthRepository: UserAuthRepository,
     val passwordEncoder: PasswordEncoder
 ) {
+    /**
+     * デバッグ用エンドポイント
+     */
+    @GetMapping("/register")
+    fun index(): List<UserAuth> {
+        return userAuthRepository.findAll()
+    }
+
     @PostMapping("/register")
     fun register(@RequestBody form: UserAuthForm): ResponseEntity<Any> {
         val existingUser = userAuthRepository.findByUsername(form.username)
