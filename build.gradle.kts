f4af5359ca2b9ab8c0faaeca224eb47bcc7aae2f
Hiroaki Date <hiroaki.date@crowdworks.co.jp>
2023-04-07 21:44:48 +0900

    JWTを使うための必要な依存関係に追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
